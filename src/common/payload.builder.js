ff3a8c6ae2a21929b9a1ded8cfa627e38bea1da5
vs4vijay <vs4vijay@gmail.com>
2020-02-10 23:42:44 +0530

    Update Payload Builder

--- src/common/payload.builder.js
+++ src/common/payload.builder.js
@@ -9,7 +9,7 @@ class PayloadBuilder {
 
   constructor() { }
 
-  generatePeerId() {
+  static generatePeerId() {
     let buffer = crypto.randomBytes(20)
     const torrentClientName = `-${CONFIG.nick}000${CONFIG.version}-`;
     console.log('torrentClientName', torrentClientName);
@@ -18,7 +18,88 @@ class PayloadBuilder {
     return buffer;
   }
 
-  buildConnectPayload() {
+  static buildHandshake(torrent) {
+    // Format: pstrlen + pstr + reserved + info_hash + peer_id
+    const buffer = Buffer.alloc(68);
+    const pstr = "BitTorrent protocol"; // Protocol Identifier
+
+    // Add protocol identifier length
+    buffer.writeUInt8(pstr.length, 0);
+
+    // Add protocol identifier
+    buffer.write(pstr, 1);
+
+    // Reserved for future use
+    buffer.writeUInt32BE(0, 20);
+    buffer.writeUInt32BE(0, 24);
+
+    // Add info hash
+    torrent.getInfoHash().copy(buf, 28);
+
+    // Add peer id
+    buffer.write(this.generatePeerId());
+    return buffer;
+  }
+
+  static buildMessage(type) {
+    // type: 
+    const buffer = Buffer.alloc(5);
+
+    buffer.writeUInt32BE(1, 0);
+
+    switch (type) {
+      case 'choke':
+        buffer.writeUInt32BE(0, 4);
+        break;
+      
+      case 'unchoke':
+        buffer.writeUInt32BE(1, 4);
+        break;
+      
+      case 'interested':
+        buffer.writeUInt32BE(2, 4);
+        break;
+      
+      case 'uninterested':
+        buffer.writeUInt32BE(3, 4);
+        break;
+      
+      default:
+        break;
+    }
+
+  }
+
+  static buildKeepAlive() {
+    const buffer = Buffer.alloc(4);
+    return buffer;
+  }
+
+  static buildHave() {
+
+  }
+
+  static buildBitfield() {
+    
+  }
+
+  static buildRequest() {
+    
+  }
+
+  static buildPiece() {
+    
+  }
+
+  static buildCancel() {
+    
+  }
+
+  static buildPort() {
+    
+  }
+
+  static buildConnectPayload() {
     // Format: connection id (8 byte) + action (4 byte) + transactionc id (4 byte)
     const buffer = Buffer.alloc(16);
 
